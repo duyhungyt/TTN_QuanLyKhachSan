5d8752aa3d168b4bd6a000ef6603aa5d00d6a6ab
duyhungyt <duyhung.k44@gmail.com>
2016-09-15 21:19:13 +0700

    Tao du an

---
+++ TTN_QuanLyKhachSan/TTN_QuanLyKhachSan/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TTN_QuanLyKhachSan
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuChucNang = new System.Windows.Forms.MenuStrip();
+            this.hướngDẫnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnNguoiDung = new System.Windows.Forms.Button();
+            this.btnPhong = new System.Windows.Forms.Button();
+            this.btnVatDung = new System.Windows.Forms.Button();
+            this.btnThuePhong = new System.Windows.Forms.Button();
+            this.btnKhachHang = new System.Windows.Forms.Button();
+            this.btnSuDungDV = new System.Windows.Forms.Button();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.btnDichVu = new System.Windows.Forms.Button();
+            this.mnuChucNang.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuChucNang
+            // 
+            this.mnuChucNang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.mnuChucNang.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hướngDẫnToolStripMenuItem});
+            this.mnuChucNang.Location = new System.Drawing.Point(0, 0);
+            this.mnuChucNang.Name = "mnuChucNang";
+            this.mnuChucNang.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.mnuChucNang.Size = new System.Drawing.Size(409, 29);
+            this.mnuChucNang.TabIndex = 0;
+            this.mnuChucNang.Text = "menuStrip1";
+            // 
+            // hướngDẫnToolStripMenuItem
+            // 
+            this.hướngDẫnToolStripMenuItem.Name = "hướngDẫnToolStripMenuItem";
+            this.hướngDẫnToolStripMenuItem.Size = new System.Drawing.Size(102, 23);
+            this.hướngDẫnToolStripMenuItem.Text = "Trợ giúp (F1)";
+            // 
+            // btnNguoiDung
+            // 
+            this.btnNguoiDung.Location = new System.Drawing.Point(31, 53);
+            this.btnNguoiDung.Name = "btnNguoiDung";
+            this.btnNguoiDung.Size = new System.Drawing.Size(164, 40);
+            this.btnNguoiDung.TabIndex = 1;
+            this.btnNguoiDung.Text = "Quản lý người dùng";
+            this.btnNguoiDung.UseVisualStyleBackColor = true;
+            // 
+            // btnPhong
+            // 
+            this.btnPhong.Location = new System.Drawing.Point(31, 99);
+            this.btnPhong.Name = "btnPhong";
+            this.btnPhong.Size = new System.Drawing.Size(164, 40);
+            this.btnPhong.TabIndex = 1;
+            this.btnPhong.Text = "Quản lý phòng";
+            this.btnPhong.UseVisualStyleBackColor = true;
+            // 
+            // btnVatDung
+            // 
+            this.btnVatDung.Location = new System.Drawing.Point(31, 145);
+            this.btnVatDung.Name = "btnVatDung";
+            this.btnVatDung.Size = new System.Drawing.Size(164, 40);
+            this.btnVatDung.TabIndex = 1;
+            this.btnVatDung.Text = "Quản lý vật dụng";
+            this.btnVatDung.UseVisualStyleBackColor = true;
+            // 
+            // btnThuePhong
+            // 
+            this.btnThuePhong.Location = new System.Drawing.Point(211, 99);
+            this.btnThuePhong.Name = "btnThuePhong";
+            this.btnThuePhong.Size = new System.Drawing.Size(164, 40);
+            this.btnThuePhong.TabIndex = 1;
+            this.btnThuePhong.Text = "Quản lý thuê phong";
+            this.btnThuePhong.UseVisualStyleBackColor = true;
+            // 
+            // btnKhachHang
+            // 
+            this.btnKhachHang.Location = new System.Drawing.Point(31, 191);
+            this.btnKhachHang.Name = "btnKhachHang";
+            this.btnKhachHang.Size = new System.Drawing.Size(164, 40);
+            this.btnKhachHang.TabIndex = 1;
+            this.btnKhachHang.Text = "Quản lý khách hàng";
+            this.btnKhachHang.UseVisualStyleBackColor = true;
+            // 
+            // btnSuDungDV
+            // 
+            this.btnSuDungDV.Location = new System.Drawing.Point(211, 145);
+            this.btnSuDungDV.Name = "btnSuDungDV";
+            this.btnSuDungDV.Size = new System.Drawing.Size(164, 40);
+            this.btnSuDungDV.TabIndex = 1;
+            this.btnSuDungDV.Text = "Quản lý sử dụng dịch vụ";
+            this.btnSuDungDV.UseVisualStyleBackColor = true;
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Location = new System.Drawing.Point(211, 191);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(164, 40);
+            this.btnThanhToan.TabIndex = 2;
+            this.btnThanhToan.Text = "Thanh Toán";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            // 
+            // btnDichVu
+            // 
+            this.btnDichVu.Location = new System.Drawing.Point(211, 53);
+            this.btnDichVu.Name = "btnDichVu";
+            this.btnDichVu.Size = new System.Drawing.Size(164, 40);
+            this.btnDichVu.TabIndex = 1;
+            this.btnDichVu.Text = "Quản lý dịch vụ";
+            this.btnDichVu.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 257);
+            this.Controls.Add(this.btnThanhToan);
+            this.Controls.Add(this.btnKhachHang);
+            this.Controls.Add(this.btnSuDungDV);
+            this.Controls.Add(this.btnDichVu);
+            this.Controls.Add(this.btnThuePhong);
+            this.Controls.Add(this.btnVatDung);
+            this.Controls.Add(this.btnPhong);
+            this.Controls.Add(this.btnNguoiDung);
+            this.Controls.Add(this.mnuChucNang);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.MainMenuStrip = this.mnuChucNang;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMain";
+            this.Text = "Main";
+            this.mnuChucNang.ResumeLayout(false);
+            this.mnuChucNang.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuChucNang;
+        private System.Windows.Forms.ToolStripMenuItem hướngDẫnToolStripMenuItem;
+        private System.Windows.Forms.Button btnNguoiDung;
+        private System.Windows.Forms.Button btnPhong;
+        private System.Windows.Forms.Button btnVatDung;
+        private System.Windows.Forms.Button btnThuePhong;
+        private System.Windows.Forms.Button btnKhachHang;
+        private System.Windows.Forms.Button btnSuDungDV;
+        private System.Windows.Forms.Button btnThanhToan;
+        private System.Windows.Forms.Button btnDichVu;
+    }
+}
+
