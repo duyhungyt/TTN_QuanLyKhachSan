94bc3c70171cc4f118250eb28a0752483fb0f05a
minhquan6595 <minhquan6595@gmail.com>
2016-10-28 15:22:57 +0700

    quanlyphong

--- TTN_QuanLyKhachSan/DAL/DAL_Phong.cs
+++ TTN_QuanLyKhachSan/DAL/DAL_Phong.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entity;
-using DAL;
 using System.Data;
 
 namespace DAL
@@ -14,23 +13,27 @@ namespace DAL
         private KetNoiData connect = new KetNoiData();
         public DataTable ThongTinPhong(string dk)
         {
-            DataTable tb = connect.GetDataTable("select * from tblPhong "+ dk );
+            DataTable tb = connect.GetDataTable("select * from tblPhong " + dk);
             return tb;
         }
 
-        public void ThemThongTin(EC_Phong EC_Ph)
+        public int ThemThongTin(EC_Phong EC_Ph)
         {
-            connect.ThucHienLenh("insert into tblPhong(MaPh,SoPhong,TrangThai) values(N'" + EC_Ph.MaPh + "', N'" + EC_Ph.SoPhong + "', N'" + EC_Ph.TrangThai + "'");
+            return connect.ThucHienLenh("insert into tblPhong(MaPh,SoPhong,TrangThai) values(N'" + EC_Ph.MaPh + "', N'" + EC_Ph.SoPhong + "', N'" + EC_Ph.TrangThai + "') ");
         }
 
-        public void SuaThongTin(EC_Phong EC_Ph)
+        public int SuaThongTin(EC_Phong EC_Ph)
         {
-            connect.ThucHienLenh("update tblPhong set SoPhong = N'" + EC_Ph.SoPhong + "', TrangThai = N'" + EC_Ph.TrangThai + "'  where MaPh = '" + EC_Ph.MaPh + "'");
+            return connect.ThucHienLenh("update tblPhong set SoPhong = N'" + EC_Ph.SoPhong + "', TrangThai = N'" + EC_Ph.TrangThai + "'  where MaPh = '" + EC_Ph.MaPh + "'");
         }
 
-        public void XoaTTPhong(EC_Phong EC_Ph)
+        public int XoaTTPhong(EC_Phong EC_Ph)
         {
-            connect.ThucHienLenh("delete from tblPhong where MaPh = '" + EC_Ph.MaPh + "'");
+            return connect.ThucHienLenh("delete from tblPhong where MaPh = '" + EC_Ph.MaPh + "'");
+        }
+        public DataTable TaoBang(string DieuKien)
+        {
+            return connect.GetDataTable(@"SELECT * FROM tblPhong " + DieuKien);
         }
     }
 }
